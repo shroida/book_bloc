cd0ac5a7ed3dedaa7caa3364ef30873e0457026b
shroida <shroida012@gmail.com>
2024-10-25 08:53:24 +0200

    add google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:book_bloc/constants.dart';
 import 'package:book_bloc/features/splash/presentation/views/splash_views.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)
+
       ),
+      
       home: const SplashView(),
     );
   }
