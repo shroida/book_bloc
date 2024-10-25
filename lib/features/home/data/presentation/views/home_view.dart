6a0bea9849f25cb0c41fd65e5284a63b43c798bf
shroida <shroida012@gmail.com>
2024-10-25 08:18:03 +0200

    custom appbar

--- lib/features/home/data/presentation/views/home_view.dart
+++ lib/features/home/data/presentation/views/home_view.dart
@@ -1,11 +1,13 @@
+import 'package:book_bloc/features/home/data/presentation/views/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
-const HomeView({ super.key });
+  const HomeView({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return const Scaffold(body: 
-    Text('Shroida'),);
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: HomeViewBody(),
+    );
   }
 }
---
+++ lib/features/home/data/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,17 @@
+import 'package:book_bloc/features/home/data/presentation/views/widgets/custom_appbar.dart';
+import 'package:flutter/material.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        CustomAppBar(),
+      ],
+    );
+  }
+}
+
+
