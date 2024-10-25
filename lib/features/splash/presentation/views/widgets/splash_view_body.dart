0e33485f236e33c0a23e7dc2793ae02466ed72f0
shroida <shroida012@gmail.com>
2024-10-25 07:15:28 +0200

    add text to splash view

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,13 @@
+import 'package:book_bloc/core/utlis/assets_images.dart';
 import 'package:flutter/material.dart';
 
 class SplashViewBody extends StatelessWidget {
-const SplashViewBody({ super.key });
+  const SplashViewBody({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return Container();
+  Widget build(BuildContext context) {
+    return Column(
+      children: [Image.asset(AssetsImages.logo)],
+    );
   }
 }
