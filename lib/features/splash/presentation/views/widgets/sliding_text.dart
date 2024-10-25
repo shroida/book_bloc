0c35fb400cf0c81ccd84805bb8ef524a7392aa84
shroida <shroida012@gmail.com>
2024-10-25 07:51:14 +0200

    adding animation

---
+++ lib/features/splash/presentation/views/widgets/sliding_text.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SlidingText extends StatelessWidget {
+    final Animation<Offset> slidingAnimation;
+
+   const SlidingText({super.key,required this.slidingAnimation});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: slidingAnimation,
+        builder: (context, _) {
+          return SlideTransition(
+            position: slidingAnimation,
+            child: const Text(
+              'Read Free Books',
+              textAlign: TextAlign.center,
+            ),
+          );
+        });
+  }
+}
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,30 @@
 import 'package:book_bloc/core/utlis/assets_images.dart';
+import 'package:book_bloc/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,14 +33,23 @@ class SplashViewBody extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsImages.logo),
-        const SizedBox(
-          height: 4,
-        ),
-        const Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-        ),
+        const SizedBox(height: 4),
+        SlidingText(
+          slidingAnimation: slidingAnimation,
+        )
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero).animate(
+            CurvedAnimation(parent: animationController, curve: Curves.easeIn));
+
+    // Start the animation
+    animationController.forward();
   }
 }
