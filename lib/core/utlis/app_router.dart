52a6aa9fb4ff3486b2c7da48036a58506bdc8ec4
shroida <shroida012@gmail.com>
2024-11-03 19:55:46 +0200

    create search view

--- lib/core/utlis/app_router.dart
+++ lib/core/utlis/app_router.dart
@@ -1,13 +1,16 @@
 import 'package:book_bloc/features/home/data/presentation/views/book_details_view.dart';
 import 'package:book_bloc/features/home/data/presentation/views/home_view.dart';
+import 'package:book_bloc/features/search/data/presentation/views/search_view.dart';
 import 'package:book_bloc/features/splash/presentation/views/splash_views.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
   static const kBookDetailsView = '/bookDetailsView';
+  static const kSearchView = '/searchView';
   static final router = GoRouter(routes: [
     GoRoute(path: '/', builder: (context, state) => const SplashView()),
+    GoRoute(path: kSearchView, builder: (context, state) => const SearchView()),
     GoRoute(path: kHomeView, builder: (context, state) => const HomeView()),
     GoRoute(path: kBookDetailsView, builder: (context, state) => const BookDetailsView()),
 
