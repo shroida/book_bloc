f18945c1abf6404a6496462ea26a0bc4574beef7
shroida <shroida012@gmail.com>
2024-11-03 19:47:26 +0200

    add SimilarBooksListView

--- lib/features/home/data/presentation/views/widgets/detailsWidgets/book_details_view_body.dart
+++ lib/features/home/data/presentation/views/widgets/detailsWidgets/book_details_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:book_bloc/core/utlis/styles_fonts.dart';
 import 'package:book_bloc/features/home/data/presentation/views/widgets/detailsWidgets/books_details_section.dart';
 import 'package:book_bloc/features/home/data/presentation/views/widgets/detailsWidgets/custom_details_appbar.dart';
+import 'package:book_bloc/features/home/data/presentation/views/widgets/detailsWidgets/similar_books_list_view.dart';
 import 'package:book_bloc/features/home/data/presentation/views/widgets/homeWidgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
 
@@ -10,38 +11,38 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-      child: Column(
-        children: [
-          const CustomBookDetailsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.17),
-            child: const CustomBookImage(),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          const Text(
-            'The Jungle book',
-            style: Styles.textStyle20,
-          ),
-         
-         
-          const BooksDetailsSection(),
-          const SizedBox(
-            height: 30,
-          ),
-        const  Align(
-          alignment: Alignment.centerLeft,
-            child:  Text(
-              
-              'You can also like',
-              textAlign: TextAlign.start,
-              style: Styles.textStyle14,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+        child: Column(
+          children: [
+            const CustomBookDetailsAppBar(),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: width * 0.17),
+              child: const CustomBookImage(),
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              'The Jungle Book',
+              style: Styles.textStyle20,
+            ),
+            const BooksDetailsSection(),
+            const SizedBox(
+              height: 30,
+            ),
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'You may also like',
+                textAlign: TextAlign.start,
+                style: Styles.textStyle14,
+              ),
+            ),
+            const SimilarBooksListView(),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/features/home/data/presentation/views/widgets/detailsWidgets/similar_books_list_view.dart
@@ -0,0 +1,20 @@
+import 'package:book_bloc/features/home/data/presentation/views/widgets/homeWidgets/custom_list_view_item.dart';
+import 'package:flutter/material.dart';
+
+class SimilarBooksListView extends StatelessWidget {
+  const SimilarBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: MediaQuery.of(context).size.height * 0.3,
+        child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              return const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: CustomBookImage(),
+              );
+            }));
+  }
+}
