5e6f97a8c8d39fdfe6ca9fe913af885855da3fd8
shroida <shroida012@gmail.com>
2024-11-03 21:07:24 +0200

    create failure object

--- lib/features/home/data/repos/home_repos.dart
+++ lib/features/home/data/repos/home_repos.dart
@@ -1,6 +1,8 @@
+import 'package:book_bloc/core/errors/failures.dart';
 import 'package:book_bloc/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepos {
-  Future<List<BookModel>> fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
