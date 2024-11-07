3da456b6f7530ea2e1bd220544c32aec1909d5cf
shroida <shroida012@gmail.com>
2024-11-07 07:37:50 +0200

    impliment cubit

--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,21 @@
-import 'package:bloc/bloc.dart';
 import 'package:book_bloc/features/home/data/models/book_model/book_model.dart';
-import 'package:meta/meta.dart';
+import 'package:book_bloc/features/home/data/repos/home_repos.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepos) : super(FeaturedBooksInitial());
+  final HomeRepos homeRepos;
+
+Future<void> fetchFeatureBooks()async{
+emit(FeaturedBooksLoading());
+var result = await homeRepos.fetchFeaturedBooks();
+result.fold((failure){
+  emit(FeaturedBooksFailure(failure.errMessage));
+}, (right){
+  emit(FeaturedBooksSuccess(right));
+
+}); 
+}
 }
--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_state.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_state.dart
@@ -1,6 +1,5 @@
 part of 'featured_books_cubit.dart';
 
-@immutable
 sealed class FeaturedBooksState {}
 
 final class FeaturedBooksInitial extends FeaturedBooksState {}
